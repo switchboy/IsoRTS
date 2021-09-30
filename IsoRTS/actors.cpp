switchboy/IsoRTS

commit 12d03acf06142e3ab21418d070791b906fd2eed1

Framework for command syncing - To be used to buffer and sync commands between clients - All commands are intendent to be excecuted  at the same ingame time and the same order so that the simulation is in sync without transfering all the induvidual actor/building/object data between clients

IsoRTS/actors.cpp
@@ -229,7 +229,7 @@ actors::actors(int type, cords location, int actorTeam, int actorId)
     this->isIdle = true;
     this->actorCommandGoal = { 0,0 };
     this->actorRealGoal = { 0, 0 };
-    this->buildingId = 0;
+    this->buildingId = -1;
     this->commonGoal = false;
     this->currentFrame = 0;
     this->idOfTarget = 0;
@@ -918,7 +918,7 @@ void actors::selectAndAttackNextTarget(int range) {
         }
     }
 
-    if (distanceToTarget < range) {
+    if (distanceToTarget < range && idOfTarget != -1) {
         bool attackMove = this->wasAttackMove;
         this->updateGoal(listOfActors[idOfTarget].getActorCords(), 0);
         this->setIsDoingAttack(false);

IsoRTS/IsoRTS.vcxproj
@@ -24,6 +24,7 @@
     <ClInclude Include="buildings.h" />
     <ClInclude Include="button.h" />
     <ClInclude Include="collision.h" />
+    <ClInclude Include="commandSync.h" />
     <ClInclude Include="gamestate.h" />
     <ClInclude Include="gametext.h" />
     <ClInclude Include="globalfunctions.h" />
@@ -47,6 +48,7 @@
     <ClCompile Include="buildings.cpp" />
     <ClCompile Include="button.cpp" />
     <ClCompile Include="collision.cpp" />
+    <ClCompile Include="commandSync.cpp" />
     <ClCompile Include="gamestate.cpp" />
     <ClCompile Include="gametext.cpp" />
     <ClCompile Include="globalfunctions.cpp" />

IsoRTS/IsoRTS.vcxproj.filters
@@ -78,6 +78,9 @@
     <ClInclude Include="mainMenu.h">
       <Filter>Header Files</Filter>
     </ClInclude>
+    <ClInclude Include="commandSync.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
   </ItemGroup>
   <ItemGroup>
     <ClCompile Include="main.cpp">
@@ -138,6 +141,9 @@
       <Filter>Source Files</Filter>
     </ClCompile>
     <ClCompile Include="mainMenu.cpp">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="commandSync.cpp">
       <Filter>Source Files</Filter>
     </ClCompile>
   </ItemGroup>

IsoRTS/commandSync.cpp
@@ -1,0 +1,41 @@
+#include "commandSync.h"
+
+commandSync::commandSync()
+{
+
+}
+
+void commandSync::addCommand(command givenCommand)
+{
+	listOfCommands.push_back(givenCommand);
+}
+
+std::vector<command> commandSync::getNextCommandsToExcecute(float nextCommandWindow)
+{
+	std::vector<command> commandsToBeExcecuted;
+
+	std::sort(this->listOfCommands.begin(), this->listOfCommands.end(),
+		[](const command& a, const command& b) {
+			return a.timeCommandGiven > b.timeCommandGiven;
+		});
+
+	for (int i = 0; i < this->listOfCommands.size(); i++) {
+		if (this->listOfCommands.back().timeCommandGiven < nextCommandWindow) {
+			commandsToBeExcecuted.push_back(this->listOfCommands.back());
+			this->listOfAllCommandsExcecuted.push_back(this->listOfCommands.back());
+			this->listOfCommands.pop_back();
+		}
+		else {
+			break;
+		}
+	}
+
+	return commandsToBeExcecuted;
+}
+
+std::vector<command> commandSync::getAllCommandsGiven()
+{
+	return this->listOfAllCommandsExcecuted;
+}
+
+commandSync gameDirector;

IsoRTS/commandSync.h
@@ -1,0 +1,23 @@
+#ifndef COMMANDSYNC_H
+#define COMMANDSYNC_H
+#include "globalfunctions.h"
+#include "humanReadableNames.h"
+
+class commandSync
+{
+
+public:
+	commandSync();
+	void addCommand(command givenCommand);
+	std::vector <command> getNextCommandsToExcecute(float gameTime);
+	std::vector <command> getAllCommandsGiven();
+
+private:
+	std::vector<command> listOfCommands;
+	std::vector<command> listOfAllCommandsExcecuted;
+
+};
+
+extern commandSync gameDirector;
+
+#endif

IsoRTS/gamestate.cpp
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include "commandSync.h"
 
 
 sf::VertexArray miniMapBackGroundPoints(sf::Quads, MAP_HEIGHT* MAP_WIDTH * 4);
@@ -687,44 +688,30 @@ void gameState::clickToPlaceBuilding() {
             }
             if (buildingPlacable)
             {
-                //Zet het gebouw neer
-                buildings newBuilding(this->buildingTypeSelected, this->mouseWorldPosition, static_cast<int>(listOfBuildings.size()), currentPlayer.getTeam());
-                listOfBuildings.push_back(newBuilding);
-                if (this->isPlacingBuilding)
-                {
-                    currentPlayer.substractResources(resourceTypes::resourceWood, listOfBuildingTemplates[this->buildingTypeSelected].getPriceOfBuilding().wood);
-                    currentPlayer.substractResources(resourceTypes::resourceFood, listOfBuildingTemplates[this->buildingTypeSelected].getPriceOfBuilding().food);
-                    currentPlayer.substractResources(resourceTypes::resourceStone, listOfBuildingTemplates[this->buildingTypeSelected].getPriceOfBuilding().stone);
-                    currentPlayer.substractResources(resourceTypes::resourceGold, listOfBuildingTemplates[this->buildingTypeSelected].getPriceOfBuilding().gold);
-                    for (int i = 0; i < this->selectedUnits.size(); i++)
+                //command naar lijst voor het bouwen van een gebouw
+                gameDirector.addCommand(
                     {
-                        nearestBuildingTile tempTile = findNearestBuildingTile(this->occupiedByBuildingList[this->mouseWorldPosition.x][this->mouseWorldPosition.y], this->selectedUnits[i]);
-                        if (tempTile.isSet)
-                        {
-                            if (this->selectedUnits.size() > 1)
-                            {
-                                if (listOfActors[this->selectedUnits[i]].getType() == 0 && listOfActors[this->selectedUnits[i]].getTeam() == currentPlayer.getTeam())
-                                {
-                                    listOfActors[this->selectedUnits[i]].updateGoal(tempTile.location, i / 5);
-                                    listOfActors[this->selectedUnits[i]].setCommonGoalTrue();
-                                    listOfBuildings[this->occupiedByBuildingList[this->mouseWorldPosition.x][this->mouseWorldPosition.y]].claimFreeBuiildingTile(tempTile.buildingId, listOfActors[this->selectedUnits[i]].getActorId());
-                                }
-                            }
-                            else
-                            {
-                                if (listOfActors[this->selectedUnits[i]].getType() == 0 && listOfActors[this->selectedUnits[i]].getTeam() == currentPlayer.getTeam())
-                                {
-                                    listOfActors[this->selectedUnits[i]].updateGoal(tempTile.location, i / 5);
-                                    listOfBuildings[this->occupiedByBuildingList[this->mouseWorldPosition.x][this->mouseWorldPosition.y]].claimFreeBuiildingTile(tempTile.buildingId, listOfActors[this->selectedUnits[i]].getActorId());
-                                }
-                            }
-                            if (listOfActors[this->selectedUnits[i]].getType() == 0 && listOfActors[this->selectedUnits[i]].getTeam() == currentPlayer.getTeam())
-                            {
-                                listOfActors[this->selectedUnits[i]].setIsBuildingTrue(listOfBuildings[this->occupiedByBuildingList[this->mouseWorldPosition.x][this->mouseWorldPosition.y]].getBuildingId(), tempTile.actionLocation);
-                            }
-                        }
+                        this->elapsedTime,
+                        currentPlayer.getTeam(),
+                        this->buildingTypeSelected,
+                        true,
+                        false,
+                        this->mouseWorldPosition,
+                        worldObject::none,
+                        stackOrderTypes::none,
+                        actionTypes::none
+                    }
+                );
+
+
+                for (int i = 0; i < this->selectedUnits.size(); i++)
+                {
+                    if (listOfActors[this->selectedUnits[i]].getType() == 0 && listOfActors[this->selectedUnits[i]].getTeam() == currentPlayer.getTeam())
+                    {
+                        // build command
                     }
                 }
+
                 this->isPlacingBuilding = false;
                 this->mousePressOutofWorld = true;
             }
@@ -1971,6 +1958,72 @@ void gameState::createVillagerButtons(int startX, int startY, int incrementalXOf
         }
 
         villagerButtonsAreThere = true;
+    }
+}
+
+void gameState::commandExcecutor(std::vector<command> commandsToBeExcecuted)
+{
+    for (command& c : commandsToBeExcecuted) {
+
+        if (c.placingBuilding) {
+            buildings newBuilding(c.subjectId, c.commandCords, static_cast<int>(listOfBuildings.size()), c.playerId);
+            listOfBuildings.push_back(newBuilding);
+            listOfPlayers[c.playerId].substractResources(resourceTypes::resourceWood,   listOfBuildingTemplates[c.subjectId].getPriceOfBuilding().wood);
+            listOfPlayers[c.playerId].substractResources(resourceTypes::resourceFood,   listOfBuildingTemplates[c.subjectId].getPriceOfBuilding().food);
+            listOfPlayers[c.playerId].substractResources(resourceTypes::resourceStone,  listOfBuildingTemplates[c.subjectId].getPriceOfBuilding().stone);
+            listOfPlayers[c.playerId].substractResources(resourceTypes::resourceGold,   listOfBuildingTemplates[c.subjectId].getPriceOfBuilding().gold);
+        }
+        else {
+            switch (c.subjectType) {
+            case worldObject::actor:
+                if (c.actionToPerform == actionTypes::actionAttackMove) {
+
+                }
+                else {
+                    switch (c.orderType) {
+                    case stackOrderTypes::stackActionAttack:
+
+                        break;
+                    case stackOrderTypes::stackActionBuild:
+
+                        break;
+                    case stackOrderTypes::stackActionGather:
+
+                        break;
+                    case stackOrderTypes::stackActionMove:
+
+                        break;
+                    }
+                }
+                break;
+            case worldObject::building:
+                switch (c.actionToPerform) {
+                case actionTypes::actionCancelBuilding:
+
+                    break;
+                case actionTypes::actionCancelProduction:
+
+                    break;
+                case actionTypes::actionMakeGate:
+
+                    break;
+                case actionTypes::actionOpenGate:
+
+                    break;
+                case actionTypes::actionCloseGate:
+
+                    break;
+                case actionTypes::actionMakeSwordsman:
+
+                    break;
+                case actionTypes::actionMakeVillager:
+
+                    break;
+                }
+            case worldObject::object:
+                break;
+            }
+        }
     }
 }
 

IsoRTS/gamestate.h
@@ -64,6 +64,7 @@ public:
     void clickUIButton() const;
     void createFogOfWar();
     void createVillagerButtons(int startX, int startY, int incrementalXOffset, bool& villagerButtonsAreThere);
+    void commandExcecutor(std::vector<command> commandsToBeExcecuted);
     void drawActorBigSprite(int actorId);
     void drawActorStats(int actorId, int textStartX, int textStartY);
     void drawActorTitle(int actorId, int textStartX, int textStartY);

IsoRTS/globalfunctions.h
@@ -7,6 +7,7 @@
 #include <vector>
 #include <random>
 #include"SFML/Graphics.hpp"
+#include "humanReadableNames.h"
 
 const int MAP_WIDTH = 256;
 const int MAP_HEIGHT = 256;
@@ -41,6 +42,19 @@ struct actorOrBuildingPrice
     int gold;
     int productionPoints;
 };
+
+struct command {
+    float timeCommandGiven;
+    int playerId;
+    int subjectId;
+    bool placingBuilding;
+    bool isStackedCommand;
+    cords commandCords;
+    worldObject subjectType;
+    stackOrderTypes orderType;
+    actionTypes actionToPerform;
+};
+
 
 cords toWorldMousePosition(int mouseX, int mouseY);
 int roll(int min, int max);

commit 54b2e1fc83690ec5f276276b2def8fde88d94db4

Fix Index out of range when actor tries to find drop off #36
Regression caused by commit 12d03acf06142e3ab21418d070791b906fd2eed1

IsoRTS/actors.cpp
@@ -1764,7 +1764,9 @@ void actors::findNearestDropOffPoint()
             if (!this->realPath && !listOfDropOffLocations.empty())
             {
                 this->updateGoal(listOfDropOffLocations.front().actionLocation, 0);
-                listOfBuildings[this->buildingId].removeActorFromBuildingTile(this->actorId);
+                if (this->buildingId != -1) {
+                    listOfBuildings[this->buildingId].removeActorFromBuildingTile(this->actorId);
+                }
                 this->buildingId = listOfDropOffLocations.front().buildingId;
                 listOfBuildings[this->buildingId].claimFreeBuiildingTile(listOfDropOffLocations.front().tileId, this->actorId);
                 listOfDropOffLocations.pop_front();